2cee2ad1de06ca3b5c1f4ff886276edf073d0904
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-27 17:21:18 +0100

    Added initial frontend design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import AddArtists from "./pages/AddArtists";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/add-artists" element={<AddArtists />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
