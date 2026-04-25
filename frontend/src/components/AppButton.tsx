47acfebad45d9f9f78b2fa928ee6dbd884b0d765
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-04-25 12:25:55 +0200

    Frontend login page update

--- frontend/src/components/AppButton.tsx
+++ frontend/src/components/AppButton.tsx
@@ -2,13 +2,15 @@ interface AppButtonProps {
   text: string;
   width: number;
   height: number;
+  onClick?: () => void;
 }
 
-function AppButton({ text, width, height }: AppButtonProps) {
+function AppButton({ text, width, height, onClick }: AppButtonProps) {
   return (
     <button
       style={{ width: width, height: height }}
       className="bg-sky-950 text-white rounded-3xl text-2xl hover:bg-sky-900 hover:cursor-pointer m-10"
+      onClick={onClick}
     >
       {text}
     </button>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,19 @@
 import AppButton from "../components/AppButton";
 
 function Login() {
+  const BACKEND_URL = "http://localhost:8000";
+
+  const handleLogin = () => {
+    window.location.href = `${BACKEND_URL}/auth/login`;
+  };
+
   return (
     <div className="flex flex-col items-center justify-around">
       <div className="text-5xl mb-50 bg-sky-100 rounded-3xl p-20">
         Welcome to SetListify!
       </div>
       <div className="mb-20">Log in to get started</div>
-      <AppButton text="Log In" width={800} height={100} />
+      <AppButton text="Log In" width={800} height={100} onClick={handleLogin} />
     </div>
   );
 }
