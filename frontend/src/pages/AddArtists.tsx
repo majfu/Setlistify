780b5715bea3e3e16b9fae9815042fb2f0af00be
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-04-25 12:54:49 +0200

    Added an input field for adding artists

--- frontend/src/pages/AddArtists.tsx
+++ frontend/src/pages/AddArtists.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import InputField from "../components/InputField";
+
 function AddArtists() {
-  return <div>Add artists</div>;
+  const [artistInput, setArtistInput] = useState<string>("");
+
+  return (
+    <div className="flex flex-col">
+      <div className="self-center text-5xl mb-20 bg-sky-100 p-10 rounded-3xl">
+        Let me help you create a playlist!
+      </div>
+      <div className="mb-10">Type in the name of an artist:</div>
+      <InputField
+        placeholderText="Artist..."
+        userInput={artistInput}
+        setUserInput={setArtistInput}
+        width={600}
+        height={100}
+      />
+    </div>
+  );
 }
 
 export default AddArtists;
