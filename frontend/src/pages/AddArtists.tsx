f5e9f57f0e26f2fc8aa1d3bde44505fb1ac0e998
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-04-26 13:49:30 +0200

    Added and implemented artists list component

--- frontend/src/pages/AddArtists.tsx
+++ frontend/src/pages/AddArtists.tsx
@@ -1,21 +1,49 @@
 import { useState } from "react";
 import InputField from "../components/InputField";
+import ArtistsList from "../components/ArtistsList";
+import AppButton from "../components/AppButton";
 
 function AddArtists() {
   const [artistInput, setArtistInput] = useState<string>("");
+  const [artistsList, setArtistsList] = useState<string[]>([]);
+
+  const addArtistToList = () => {
+    if (!artistInput.length) return;
+    setArtistsList([...artistsList, artistInput]);
+    setArtistInput("");
+  };
+
+  const removeArtistFromList = (artist: string) => {
+    setArtistsList(artistsList.filter((a) => a !== artist));
+  };
 
   return (
     <div className="flex flex-col">
-      <div className="self-center text-5xl mb-20 bg-sky-100 p-10 rounded-3xl">
+      <div className="self-center text-5xl mb-40 bg-sky-100 p-10 rounded-3xl">
         Let me help you create a playlist!
       </div>
-      <div className="mb-10">Type in the name of an artist:</div>
-      <InputField
-        placeholderText="Artist..."
-        userInput={artistInput}
-        setUserInput={setArtistInput}
-        width={600}
-        height={100}
+      <div className="flex flex-col mb-20 gap-5">
+        <div>Type in the name of an artist:</div>
+        <div className="flex items-center gap-20 mt-5 ">
+          <InputField
+            placeholderText="..."
+            userInput={artistInput}
+            setUserInput={setArtistInput}
+            width={600}
+            height={100}
+          />
+          <AppButton
+            text="+"
+            width={60}
+            height={60}
+            onClick={addArtistToList}
+          />
+        </div>
+      </div>
+      <ArtistsList
+        listTitle="Chosen Artists:"
+        artistsList={artistsList}
+        onDelete={removeArtistFromList}
       />
     </div>
   );
