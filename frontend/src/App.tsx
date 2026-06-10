8e2afa9e76470abbe5fca94c3286a0ea0f2873ef
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-06-10 17:11:08 +0200

    Added the pages to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 import AddArtists from "./pages/AddArtists";
 import AddSongs from "./pages/AddSongs";
 import SetTitle from "./pages/SetTitle";
+import Browse from "./pages/Browse";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/add-artists" element={<AddArtists />} />
+        <Route path="/browse" element={<Browse />} />
         <Route path="/add-songs" element={<AddSongs />} />
         <Route path="/set-title" element={<SetTitle />} />
         <Route path="/404" element={<NotFound />} />
